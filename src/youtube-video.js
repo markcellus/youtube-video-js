aeea8aa8855a3fa70ca408c1d455b0e4e84c4116
Mark Kennedy <mark.kennedy@akqa.com>
2015-02-22 10:18:42 -0500

    File breakout

--- src/youtube-video.js
+++ src/youtube-video.js
@@ -11,75 +11,7 @@
 })((function (_) {
     'use strict';
 
-    var BaseVideo = function () {};
 
-    BaseVideo.prototype = {
-
-        /**
-         * Initialization.
-         * @param {object} options - Options passed into instance
-         */
-        initialize: function (options) {
-
-            var el = options.el || document.createDocumentFragment();
-
-            this.options = _.extend({
-                el: el,
-                src: el.getAttribute('src'),
-                autoplay: el.getAttribute('autoplay')
-            }, options);
-
-            BaseVideo.prototype.vidCount = BaseVideo.prototype.vidCount || 0;
-            BaseVideo.prototype.vidCount++;
-
-            this.vpid = 'v' + BaseVideo.prototype.vidCount;
-
-        },
-
-        /**
-         * Adds an event listener to the media element.
-         * @param event
-         * @param listener
-         * @param useCapture
-         */
-        addEventListener: function (event, listener, useCapture) {
-            this.el.addEventListener(event, listener, useCapture);
-        },
-
-        /**
-         * Removes an event listener fromt the media element.
-         * @param event
-         * @param listener
-         * @param useCapture
-         */
-        removeEventListener: function (event, listener, useCapture) {
-            this.el.removeEventListener(event, listener, useCapture);
-        },
-
-        load: function () {
-            this.el.load();
-        },
-
-        /**
-         * Plays the video from it’s current location.
-         */
-        play: function () {
-            this.el.play();
-        },
-
-        /**
-         * Pauses the video at the then-current time.
-         */
-        pause: function () {
-            this.el.pause();
-        },
-
-        /**
-         * Destroys the player instance.
-         */
-        destroy: function () {}
-
-    };
 
     var YoutubeVideo = function (options) {
         this.initialize(options);
