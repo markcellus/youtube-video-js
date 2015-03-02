907814e93cf7804faddb39f1a12b5947ec1dde7f
Mark Kennedy <mark.kennedy@akqa.com>
2015-03-02 13:19:19 -0500

    Adding libs

---
+++ src/libs/element-kit/element.js
@@ -0,0 +1,446 @@
+'use strict';
+
+var utils = require('./utils');
+var ElementKit = require('./element-kit');
+
+var Element = function (el) {
+    this.initialize(el);
+};
+
+/**
+ * A class from which all Elements are based.
+ * @description Bootstraps an element to allow for native JS methods (see https://developer.mozilla.org/en-US/docs/Web/API/Element)
+ * @class Element
+ * @param {Element} el - The element
+ */
+Element.prototype = /** @lends Element */{
+
+    initialize: function (el) {
+        this.el = el;
+        this.classList = this._getClassList();
+        this._eventListenerMap = this._eventListenerMap || [];
+
+        Object.defineProperty(this, 'dataset', {
+            get: function () {
+                return this.getData();
+            }.bind(this)
+        });
+    },
+
+    /**
+     * Bubbles up each parent node of the element, triggering the callback on each element until traversal
+     * either runs out of parent nodes, reaches the document element, or if callback returns a falsy value
+     * @param {Function} callback - A callback that fires which gets passed the current element
+     * @param {HTMLElement} [startEl] - The element where traversal will begin (including the passed element), defaults to current el
+     * @private
+     */
+    _traverseEachParent: function (callback, startEl) {
+        var parentNode = startEl || this.el,
+            predicate;
+        // check if the node has classname property, if not, we know we're at the #document element
+        while (parentNode && typeof parentNode.className === 'string') {
+            predicate = callback(parentNode);
+            if (predicate !== undefined && !predicate) {
+                break;
+            }
+            parentNode = parentNode.parentNode;
+        }
+    },
+
+    /**
+     * Wrap a parent container element around the element.
+     * @param {string} html - The wrapper html
+     */
+    appendOuterHtml: function (html) {
+        var parent = this.el.parentNode,
+            container = utils.createHtmlElement(html);
+        if (parent) {
+            parent.replaceChild(container, this.el);
+        } else {
+            parent = document.createDocumentFragment();
+            parent.appendChild(container);
+        }
+        container.appendChild(this.el);
+        return container;
+    },
+
+    /**
+     * Retrieves the unique identifier of the element.
+     * @private
+     */
+    getUniqueId: function () {
+        return this.el._kitId;
+    },
+
+    /**
+     * Gets the closest ancestor element that has a css class.
+     * @param {string} className - The class name that the ancestor must have to match
+     */
+    getClosestAncestorElementByClassName: function (className) {
+        var result;
+        this._traverseEachParent(function (parent) {
+            if (parent.kit._hasClass(className)) {
+                result = parent;
+                return false;
+            }
+        }, this.el.parentNode);
+        return result;
+    },
+
+    /**
+     * Adds an event listener to the element.
+     * @param {string} event - The event to listen to
+     * @param {string|Function} listener - The name of the function (or the function itself) that should fire when the event happens
+     * @param {Object} [context] - The context in which the function should be called
+     * @param {Object} [options] - Object containing additional options
+     * @param {Object} [options.useCapture] - Whether to use capture (see Web.API.EventTarget.addEventListener)
+     */
+    addEventListener: function (event, listener, context, options) {
+        var _listener = listener;
+        options = options || {};
+
+        if (typeof _listener !== 'function') {
+            _listener = this._createEventListener(context[listener], context);
+        }
+
+        this.el.addEventListener(event, _listener, options.useCapture);
+
+        this._eventListenerMap.push({
+            event: event,
+            listener: _listener,
+            listenerId: listener,
+            context: context
+        });
+    },
+
+    /**
+     * Creates an event listener bounded to a context (useful for adding and removing events).
+     * @param {Function} listener - The listener function
+     * @param {Object} context - The context that should be used when the function is called
+     * @returns {Function} Returns an event listener function bounded to the context
+     * @private
+     */
+    _createEventListener: function (listener, context) {
+        return function (e) {
+            context = context || this;
+            listener.apply(context, arguments);
+        }
+    },
+
+    /**
+     * Removes an event listener from the element.
+     * @param {string} event - The event to remove
+     * @param {string|Function} listener - The event listener function or (name of it) to be removed
+     * @param {Object} [context] - The context of the listener that is being removed
+     */
+    removeEventListener: function (event, listener, context) {
+        var map = this._eventListenerMap || [],
+            i,
+            obj;
+
+        if (map.length) {
+            for (i = 0; i < map.length; i++) {
+                obj = map[i];
+                if (obj && obj.event === event && obj.listenerId === listener && obj.context === context) {
+                    this.el.removeEventListener(event, obj.listener);
+                    this._eventListenerMap[i] = null;
+                    break;
+                }
+            }
+        }
+    },
+
+    /**
+     * Builds a transition promise that waits to resolve until the el's CSS transition is completed.
+     * @param {Function} callback - The callback that is fired when the transition time is complete
+     * @returns {HTMLElement} Returns the html element
+     */
+    waitForTransition: function (callback) {
+        var duration = this.getTransitionDuration();
+        if (callback) {
+            if (duration > 0) {
+                setTimeout(callback.bind(this, this.el), duration);
+            } else {
+                callback(this.el);
+            }
+        }
+    },
+
+    /**
+     * Gets the time is takes for the element to transition to its show state.
+     * @returns {Number} Returns the total CSS transition time in milliseconds
+     */
+    getTransitionDuration: function () {
+        var delayProp = this.getCssComputedProperty('transition-delay') || '0ms',
+            durationProp = this.getCssComputedProperty('transition-duration') || '0ms',
+            times = Array.isArray(durationProp) ? durationProp : [durationProp],
+            delay = Array.isArray(delayProp) ? delayProp : [delayProp],
+            highest = 0,
+            map;
+
+        times.push.apply(times, delay); // account for delay
+
+        // calculate highest number of time
+        times.forEach(function (value) {
+            value.split(',').forEach(function (v) {
+                v = this._convertCssTimeValueToMilliseconds(v);
+                map = this._getCssPropUnitMap(v);
+                if (map.num > highest) {
+                    highest = map.num;
+                }
+            }.bind(this));
+        }.bind(this));
+
+        return highest;
+    },
+
+    /**
+     * Gets the computed property of the element.
+     * @param {string} prop - The name of the property to get
+     * @returns {string} Returns the value of the property
+     */
+    getCssComputedProperty: function (prop) {
+        var style = window.getComputedStyle(this.el);
+        return style.getPropertyValue(prop) || this.el.style[this._getJsPropName(prop)];
+    },
+
+    /**
+     * Takes a value and separates the number and unit into a key/value map.
+     * @param v - The value
+     * @returns {{num: Number, unit: string}} Returns the map
+     * @private
+     */
+    _getCssPropUnitMap: function (v) {
+        v.trim();
+        var num = v.match('[0-9\.]+'),
+            unit = 'ms';
+
+        num = num ? num[0] : '';
+        if (num) {
+            unit = v.split(num)[1];
+            num = Number(num);
+        }
+        return {
+            num: num,
+            unit: unit
+        };
+    },
+
+    /**
+     * Converts a css timing unit value into milliseconds.
+     * @param {string} val - The value string
+     * @returns {string} Returns the timing unit value in milliseconds
+     * @private
+     */
+    _convertCssTimeValueToMilliseconds: function (val) {
+        var number = this._getCssPropUnitMap(val).num,
+            unit = val.replace(number, '');
+        if (unit === 's') {
+            val = number * 1000;
+        } else {
+            val = number;
+        }
+        return val + 'ms';
+    },
+
+    /**
+     * Gets the class list of an element.
+     * @returns {Array} Returns an array of class names.
+     * @private
+     */
+    _getClassList: function () {
+        return {
+            add: this._addClass.bind(this),
+            remove: this._removeClass.bind(this),
+            contains: this._hasClass.bind(this),
+            toggle: this._toggleClass.bind(this)
+        };
+    },
+
+    /**
+     * Gets the class list of an element.
+     * @returns {Array} Returns an array of class names.
+     * @private
+     */
+    _getCssClasses: function () {
+        return this.el.className.split(' ');
+    },
+
+    /**
+     * Toggles (adds/removes) a css class on the element.
+     * @param {string} className - The css class value to add/remove
+     * @private
+     */
+    _toggleClass: function (className) {
+        if (!this._hasClass(className)) {
+            this._addClass(className);
+        } else {
+            this._removeClass(className);
+        }
+    },
+
+    /**
+     * Adds a CSS class to the element.
+     * @param {...string} arguments - The arguments containing css classes to add
+     * @private
+     */
+    _addClass: function  () {
+        if (('classList' in document.createElement('_'))) {
+            // browser supports classList!
+            this._each(arguments, function (className) {
+                this.el.classList.add(className);
+            }.bind(this));
+        } else {
+            this._each(arguments, function (className) {
+                if (!this._hasClass(className)) {
+                    this.el.className = this.el.className ? this.el.className + ' ' + className : className;
+                }
+            }.bind(this));
+        }
+    },
+
+    /**
+     * Triggers a callback function for a set of items.
+     * @param {Array} items - An array of items
+     * @param {Function} method - The function to execute for each item
+     * @private
+     */
+    _each: function (items, method) {
+        var count = items.length,
+            i;
+        for (i = 0; i < count; i++) {
+            method(items[i]);
+        }
+    },
+
+    /**
+     * Removes a CSS class from the element.
+     * @param {...string} arguments - The arguments containing css classes to remove
+     * @private
+     */
+    _removeClass: function () {
+        var re;
+        if ('classList' in document.createElement('_')) {
+            this._each(arguments, function (className) {
+                this.el.classList.remove(className);
+            }.bind(this));
+        } else {
+            this._each(arguments, function (className) {
+                if (this.el.className === className) {
+                    // if the only class that exists,  remove it and make empty string
+                    this.el.className = '';
+                } else {
+                    re = '[\\s]*' + className;
+                    re = new RegExp(re, 'i');
+                    this.el.className = this.el.className.replace(re, '');
+                }
+            }.bind(this));
+        }
+    },
+
+    /**
+     * Checks if the element has a class.
+     * @param {string} className - The css class value to check
+     * @private
+     */
+    _hasClass: function (className) {
+        var classes = this._getCssClasses();
+        return classes.indexOf(className) !== -1;
+    },
+
+    /**
+     * Takes a css property name and returns the javascript version of it.
+     * @param {string} cssProp - The css property
+     * @returns {string} Returns the javascript version
+     * @private
+     */
+    _getJsPropName: function (cssProp) {
+        // convert to camelCase
+        cssProp = cssProp.replace(/-([a-z])/g, function (letter) {
+            return letter[1].toUpperCase();
+        });
+        return cssProp;
+    },
+
+    /**
+     * Gets a simplified mapping of all attributes of an element.
+     * @returns {object} - Returns an object containing all attribute mappings
+     */
+    getAttributes: function () {
+        var attrs = this.el.attributes,
+            map = {};
+        if (attrs.length) {
+            for (var i = 0; i < attrs.length; i++) {
+                map[attrs[i].name] = attrs[i].value;
+            }
+        }
+        return map;
+    },
+
+    /**
+     * Gets the elements current data attributes that have been assigned in the DOM.
+     * @returns {{}}
+     * @private
+     */
+    _getDomData: function () {
+        var attrs = this.getAttributes(), data = {}, key, value;
+        for (key in attrs) {
+            if (attrs.hasOwnProperty(key)) {
+                value = attrs[key];
+                if (key.indexOf('data-') === 0) {
+                    // data attribute found!
+                    key = key.substr(5);
+                    data[key] = value;
+                }
+            }
+        }
+        return data;
+    },
+
+    /**
+     * Returns an object of the element's current data attributes.
+     * @returns {*|{}}
+     */
+    getData: function () {
+        var key;
+
+        this._data = utils.extend({}, this._data, this._getDomData());
+
+        // convert all current data properties to be "watchable".
+        for (key in this._data) {
+            if (this._data.hasOwnProperty(key)) {
+                var value = this._data[key];
+                // TODO: we should only convert it if it isnt already a "watchable" obj
+                Object.defineProperty(this._data, key, {
+                    writeable: true,
+                    get: function () {
+                        return value;
+                    }.bind(this),
+                    set: function (value) {
+                        this.setData.bind(this, key, value)
+                    }.bind(this)
+                });
+            }
+        }
+        return this._data;
+
+    },
+
+    /**
+     * When data is being set.
+     * @param {string} key - The key of which to be set
+     * @param {*} value - The value
+     */
+    setData: function (key, value) {
+        this.el.setAttribute('data-' + key, value);
+        this._data[key] = value;
+
+    },
+
+    /**
+     * Destroys the kit on the element.
+     */
+    destroy: function () {}
+};
+
+module.exports = Element;
---
+++ src/libs/element-kit/utils.js
@@ -0,0 +1,37 @@
+module.exports = {
+    /**
+     * Creates an HTML Element from an html string.
+     * @param {string} html - String of html
+     * @returns {HTMLElement} - Returns and html element node
+     */
+    createHtmlElement: function (html) {
+        var tempParentEl,
+            el;
+        if (html) {
+            html = html.trim(html);
+            tempParentEl = document.createElement('div');
+            tempParentEl.innerHTML = html;
+            el = tempParentEl.childNodes[0];
+            return tempParentEl.removeChild(el);
+        }
+    },
+
+    /**
+     * Merges the contents of two or more objects.
+     * @param {object} obj - The target object
+     * @param {...object} - Additional objects who's properties will be merged in
+     */
+    extend: function (target) {
+        var merged = target,
+            source, i;
+        for (i = 1; i < arguments.length; i++) {
+            source = arguments[i];
+            for (var prop in source) {
+                if (source.hasOwnProperty(prop)) {
+                    merged[prop] = source[prop];
+                }
+            }
+        }
+        return merged;
+    }
+};
