57ee298a7317c28d62037db620144503b4c2198e
Mark <mark@heyimmark.com>
2018-09-08 18:48:22 -0400

    Support UMD consumption + make esm default

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const formats = [
+    {format: 'esm', extension: 'js'},
+    {format: 'umd', extension: 'umd.js'},
+];
+
+const configs = formats.map(({format, extension}) => {
+    const filePath = 'youtube-video';
+    return {
+        input: `src/${filePath}.js`,
+        output: {
+            name: 'YoutubeVideo ',
+            format,
+            file: `dist/${filePath}.${extension}`
+        }
+    };
+});
+
+export default configs;
