b03ede112f6b24d482707410487fd311575ade0c
Mark <mark@heyimmark.com>
2019-08-21 07:57:28 -0400

    Use rollup-plugin-serve

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
+import serve from 'rollup-plugin-serve';
 
 export default {
     input: 'src/youtube-video.ts',
@@ -7,7 +8,16 @@ export default {
         format: 'esm',
         file: 'dist/youtube-video.js',
     },
-    plugins: [resolve(), typescript()],
+    plugins: [
+        resolve(),
+        typescript(),
+        process.env.ROLLUP_WATCH &&
+            serve({
+                historyApiFallback: true,
+                contentBase: '',
+                port: 3139,
+            }),
+    ],
     watch: {
         include: 'src/**',
     },
