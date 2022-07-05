52a385928fca76267214ba12500722eed0ebd066
mark <1446916+markcellus@users.noreply.github.com>
2022-07-05 10:36:05 -0400

    Add tests and use examples

--- tests/youtube-video-tests.e2e.js
+++ tests/youtube-video-tests.e2e.js
@@ -1,13 +1,13 @@
 import { Selector } from 'testcafe';
 
-fixture`Youtube Video Tests`.page`http://localhost:2201/youtube-video.html`;
+fixture`Youtube Video Tests`
+    .page`http://localhost:2201/examples/youtube-video.html`;
 
-test('changes displayed content to "playing" when you click play', async (t) => {
+test('changes displayed content to "playing" when video is played', async (t) => {
     const body = Selector('body');
     const display = body.find('.video-state');
-    const video = body.find('youtube-video');
-
     await t.expect(display.textContent).notEql('playing');
-    await t.click(video); // click video PLAY button here
+    const video = body.find('youtube-video');
+    await t.click(video);
     await t.expect(display.textContent).eql('playing');
 });
